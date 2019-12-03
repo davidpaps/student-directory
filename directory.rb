f293cfa61cd6a45ecd2246717673856a0e2777f8
David Papamichael <davidpapamichael@gmail.com>
2019-12-03 14:56:57 +0000

    8.3

--- directory.rb
+++ directory.rb
@@ -25,7 +25,7 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].index("d") == 0
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < 12
   end
 end
 
