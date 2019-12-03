d4c4872f31e3846337ff610c18567b533f2a58f5
David Papamichael <davidpapamichael@gmail.com>
2019-12-03 18:05:12 +0000

    8.7

--- directory.rb
+++ directory.rb
@@ -1,59 +1,70 @@
 def input_students
 
+    months = {
+        "" => :na,
+        "January" => :january,
+        "February" => :february,
+        "March" => :march,
+        "April" => :april,
+        "May" => :may,
+        "June" => :june,
+        "July" => :july,
+        "August" => :august,
+        "September" => :september,
+        "October" => :october,
+        "November" => :november,
+        "December" => :december
+    }
+    puts "Please enter the following information:"
+    puts "To finish just hit return twice!"
+
     students = []
 
-    while true do
-        puts "Type 'yes' to create a Student Profile? (To finish just hit 'return')"
-        student_profile = gets.chomp
-        if !student_profile.empty?
-            puts "Please enter the information:"
-
-            puts "Please enter your name:"
-            name = gets.chomp
-
-            puts "What are your hobbies:"
-            hobbies = gets.chomp
-
-            puts "where were you born:"
-            country = gets.chomp
-
-            puts "How tall are you?"
-            height = gets.chomp
-
-            students << {name: name, hobbies: hobbies, country: country, height: height, cohort: :november}
-
-            puts "Now we have #{students.count} students"
-        else
-            break
-        end
+    puts "Please enter your name:"
+    name = gets.chomp
+    if name.empty?
+        name = "None"
     end
-  students
+
+    puts "Please enter your cohort:"
+    cohort = months[gets.chomp.capitalize]
+    while cohort == nil
+        puts "Try again, please enter your cohort:"
+        cohort = months[gets.chomp.capitalize]
+    end
+
+
+    while !name.empty? do
+
+      students << {name: name, cohort: cohort}
+      puts "Now we have #{students.count} students"
+
+      name = gets.chomp
+    end
+
+    students
 end
 
-def print_header
-  puts "The students of Villains Academy".center(50)
-  puts "-------------".center(50)
-end
+  def print_header
+    puts "The students of Villains Academy".center(50)
+    puts "-------------".center(50)
+  end
 
-def print(students)
-    counter = 0
-    until counter == students.length do
+  def print(students)
+    students.each do |student|
         puts %{
-                Name: #{students[counter][:name]}
-                Hobby: #{students[counter][:hobbies]}
-                Country: #{students[counter][:country]}
-                Height: #{students[counter][:height]}
-                Cohort: #{students[counter][:cohort]} cohort}.center(50)
-        counter += 1
-    end
-end
+            Name: #{student[:name]}
+            Cohort: #{student[:cohort]}}
+        end
+  end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students".center(50)
-end
+  def print_footer(names)
+    puts "Overall, we have #{names.count} great students".center(50)
+  end
 
-students = input_students
+  students = input_students
 
-print_header
-print(students)
-print_footer(students)
+  print_header
+  print(students)
+  print_footer(students)
+  
